e97556b5c0b342142a8ae0bb4eb25458222af8e9
Gabriel Porto <gabrielporto1903@gmail.com>
2019-07-18 23:43:04 -0300

    add input and state robots

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-const readline = require('readline-sync')
 const robots = {
-    text: require('./robots/text.js')
+    text: require('./robots/text.js'),
+    input: require('./robots/input.js'),
+    state: require('./robots/state.js')
 }
 
 async function start() {
-    const content = {
-        maximumSentences: 7
-    }
+    
+    robots.input()
+    await robots.text()
 
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-    content.language = askAndReturnLanguage()
-    await robots.text(content)
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixedIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixedText = prefixes[selectedPrefixedIndex]
-
-        return selectedPrefixedText
-    }
-
-    function askAndReturnLanguage() {
-        const languages = ['pt', 'en']
-        const selectedLanguageIndex = readline.keyInSelect(languages, 'Choose one option: ')
-        const selectedLanguageText = languages[selectedLanguageIndex]
-
-        return selectedLanguageText
-    }
-
-    console.log(JSON.stringify(content, null, 4))
+    const content = robots.state.load()
+    console.dir(content, { depth: null })
 }
 
 start()
