de5c800c44ba9662eea63817128a3cd28c597a92
Gabriel Porto <gabrielporto1903@gmail.com>
2019-07-26 23:10:33 -0300

    add images download

--- robots/image.js
+++ robots/image.js
@@ -9,9 +9,9 @@ async function robot() {
 
     const content = state.load()
 
-   // await fetchImagesOfAllSentences(content)
+    await fetchImagesOfAllSentences(content)
     await downloadAllImages(content)
-   // state.save(content)
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
       for (const sentence of content.sentences) {
